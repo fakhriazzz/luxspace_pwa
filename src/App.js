41abdc399aee39d60f510ce6f7c46418de468710
Fakhri Azmi Husaini <rifakhrii@gmail.com>
2024-05-07 14:57:46 +0700

    Header dan hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './components/Header.js'
+import Hero from './components/Hero.js';
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello
-    </h1>
+    <>
+      <Header />
+      <Hero />
+    </>
   );
 }
 
